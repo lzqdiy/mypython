5f109e6c484e553c647f8b2b5c2b74947a727131
lzqdiy <lzqdiy@163.com>
2026-06-02 14:34:30 +0900

    修改网页标题为'我的加密货币'

--- app_fastapi.py
+++ app_fastapi.py
@@ -15,7 +15,7 @@ for stream in (sys.stdout, sys.stderr):
         stream.reconfigure(encoding="utf-8", errors="replace")
 
 # 初始化FastAPI应用
-app = FastAPI(title="加密货币价格预测")
+app = FastAPI(title="我的加密货币")
 
 
 # 请求模型
